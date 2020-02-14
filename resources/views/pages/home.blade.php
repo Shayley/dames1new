06eb0f68cd87bfd8617dfc9f185eeded4ca80065
Shayley <47244034+Shayley@users.noreply.github.com>
2020-02-14 16:04:56 +0100

    Changes to navbar and header
    Name says it

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,10 +2,21 @@
 
 @section('content')
 
-    <script src="{{asset('js/hero.js')}}"></script>
+    <div id="container">
+
+        <div id="bg">
+            <div id="color-overlay"></div>
+        </div>
+
+        <div class="sitetitle">
+            <h1>vv Heerde Dames 1</h1>
+        </div>
+
+    </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/1.1.1/typed.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script>
+    <script src="https://npmcdn.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
 
 @endsection
